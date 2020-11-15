32980271cca495c1a2b22c1fec3a9d8b2a1e0089
Jordan Koehler <digipower66@gmail.com>
2020-11-15 13:44:33 -0500

    slider now toggles back and forth

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-const checkbox = document.querySelector("label");
-const inputCheck = document.getElementById("sliderCheck");
+const inputCheck = document.getElementById("checkbox");
 
 const body = document.querySelector("body");
 const socialCard = document.querySelectorAll(".social-card");
 const overviewCard = document.querySelectorAll(".overview-card");
-console.log(slider);
 
-checkbox.addEventListener("click", function () {
-  if ((inputCheck.checked = false)) inputCheck.checked = true;
-
-  //once checked slider moves
-  inputCheck.checked = true;
-
+inputCheck.addEventListener("click", function () {
   //Text Font, background color and slider colour changes
   body.classList.toggle("darkTheme");
   socialCard.forEach(function (card) {
